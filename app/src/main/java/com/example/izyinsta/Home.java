aaeb15159ea693b6edd6ebf5a36e76e494247884
Nihlorion <eskimo241pro@gmail.com>
2025-01-26 11:50:30 +0100

    Saving Credentials on client + auto login

--- app/src/main/java/com/example/izyinsta/Home.java
+++ app/src/main/java/com/example/izyinsta/Home.java
@@ -1,8 +1,11 @@
 package com.example.izyinsta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,15 +22,22 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.json.JSONObject;
 import org.json.JSONException;
+import android.content.Context;
+
+
+
 public class Home extends AppCompatActivity {
 
     private EditText username;
     private EditText password;
-    private EditText email;
     private TextView dbgText;
 
-    //public String servUrl = "http://android.chocolatine-rt.fr:7217/androidServ/";
-    public String servUrl = "http://10.192.16.90/androidServ/";
+    public String servUrl = "http://android.chocolatine-rt.fr:7217/androidServ/";
+    //public String servUrl = "http://10.192.16.90/androidServ/";
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,11 +47,27 @@ public class Home extends AppCompatActivity {
         password = findViewById(R.id.textMDPInput);
 
         dbgText = findViewById(R.id.dbgText);
+
+        //Checking if client has previously logged in using shared preferences
+        SharedPreferences preferences = getSharedPreferences("com.example.izyinsta.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE);
+        String savedUsername = preferences.getString("username", "");
+        String savedPassword = preferences.getString("password", "");
+
+        //Debugging, you can see the clear password lol, pretty secure
+        Log.d("DBG", "savedUsername: "+savedUsername);
+        Log.d("DBG", "savedPassword: "+savedPassword);
+
+        //If the client has previously logged in, we log him in automatically
+        if (!savedUsername.equals("") && !savedPassword.equals("")) {
+            username.setText(savedUsername);
+            password.setText(savedPassword);
+            register(null);
+        }
+
     }
 
     public void register (View v) {
         OkHttpClient client = new OkHttpClient();
-        String url = servUrl + "index.php";
 
         RequestBody body = new FormBody.Builder()
                 .add("username", username.getText().toString())
@@ -54,42 +80,50 @@ public class Home extends AppCompatActivity {
                 .post(body)
                 .build();
 
-        Log.d("fetchPost", "fetchPost: "+request.toString());
+        Log.d("fetchPost", "fetchPost: "+request);
         client.newCall(request).enqueue(new okhttp3.Callback() {
             @Override
-            public void onFailure(okhttp3.Call call, IOException e) {
+            public void onFailure(@NonNull okhttp3.Call call, @NonNull IOException e) {
                 Log.d("fetchPost", "onFailure: "+e.getMessage());
             }
 
+            @SuppressLint("SetTextI18n")
             @Override
-            public void onResponse(Call call, Response response) throws IOException {
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                 if (response.isSuccessful()) {
+                    assert response.body() != null;
                     final String myResponse = response.body().string();
-                    Home.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                JSONObject obj = new JSONObject(myResponse);
-                                if (obj.getString("success").equals("1")) {
-                                    Intent intent = new Intent(Home.this, devSend.class);
-                                    startActivity(intent);
-                                }
-                                else {
-                                    Log.d("DBG", "LoginFailed");
-                                    dbgText.setText("Incorrect username or password");
-                                }
-                            } catch (JSONException e) {
-                                throw new RuntimeException(e);
-                            }
+                    Home.this.runOnUiThread(() -> {
+                        try {
+                            JSONObject obj = new JSONObject(myResponse);
+                            if (obj.getString("success").equals("1")) {
 
-                            //dbgText.setText(myResponse);
-                            Log.d("TAG", "Login Response: "+myResponse);
+                                //If login is successful, we save the username and password in shared preferences
+                                SharedPreferences preferences = getSharedPreferences("com.example.izyinsta.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = preferences.edit();
+                                editor.putString("username", username.getText().toString());
+                                editor.putString("password", password.getText().toString());
+                                editor.apply();
+
+                                //Redirect to main activity
+                                Intent intent = new Intent(Home.this, devSend.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                Log.d("DBG", "LoginFailed");
+                                dbgText.setText("Nom d'utilisateur ou mot de passe incorrect");
+                            }
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
                         }
+
+                        //dbgText.setText(myResponse);
+                        Log.d("TAG", "Login Response: "+myResponse);
                     });
                 }
                 else {
-                    Log.d("fetchPost", "onResponse: "+response.toString());
-                    dbgText.setText("Error connecting to the server");
+                    Log.d("fetchPost", "onResponse: "+response);
+                    dbgText.setText("Connection au serveur impossible");
                 }
 
             }
